5b50f99173e26946747e5661216ef84832be6802
Светлана Анненкова <m2111718@edu.misis.ru>
2023-01-29 15:58:33 +0500

    Initial commit

---
+++ src/test/java/ru.misis.coursework/CourseprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.misis.courseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
